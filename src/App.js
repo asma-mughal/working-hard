f3abd2f2c60b3da2d952775dec34686a4950290a
Muhammad Yaseen <Yaseen@Muhammads-MacBook-Pro.local>
2023-12-08 17:04:17 +0500

    Work

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Practice from './Practice';
+import Section2 from './Section2';
+import MyModal from './Dialogue';
+import ExampleTabs from './Tabs';
+import MyTabs from './Tabs';
+import Example from './Tabs';
+
+function App() {
+  return (
+    <>
+
+  <Practice />
+<div className='bg-red-500'>
+<ExampleTabs />
+</div>
+  </>
+  );
+}
+
+export default App;
